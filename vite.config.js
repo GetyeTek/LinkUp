d79ea4e2aa36385e33edc40409499d45a4867ca5
GetyeTek <getyeteklu2@gmail.com>
2026-06-28 10:40:38 +0300

    Patch: 19 operations

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,24 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/LinkUp/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('packages/gibi-news')) {
+            return 'module-gibi-news';
+          }
+          if (id.includes('packages/heaven-academy')) {
+            return 'module-heaven-academy';
+          }
+          if (id.includes('packages/squad')) {
+            return 'module-squad';
+          }
+          if (id.includes('node_modules')) {
+            return 'vendor-core';
+          }
+        }
+      }
+    }
+  }
 })
