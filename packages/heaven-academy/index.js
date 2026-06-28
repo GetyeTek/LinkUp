5455a0330281541957b1bf6d51291d3b38bd7246
GetyeTek <getyeteklu2@gmail.com>
2026-06-28 12:29:02 +0300

    Wave 3: Turborepo Orchestration & Component Federation

--- packages/heaven-academy/index.js
+++ packages/heaven-academy/index.js
@@ -1,2 +1,8 @@
 import Study from './Study.jsx';
+import { registerComponent } from '@linkup/core-sdk';
+import { renderBookBlock } from './BookReader/subjects/Registry.jsx';
+
+// Register the proprietary textbook renderer so the Host can use it dynamically
+registerComponent('book-block-renderer', renderBookBlock);
+
 export default Study;
