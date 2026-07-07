ba54827f8809366961700d5b1a044b16f557c121
GetyeTek <getyeteklu2@gmail.com>
2026-07-07 16:52:54 +0300

    Extract Web Audio processing and Pinch-to-Zoom physics into

--- packages/core-sdk/src/index.js
+++ packages/core-sdk/src/index.js
@@ -1,3 +1,4 @@
 export { supabase } from './supabaseClient.js';
 export { PlatformProvider, usePlatform } from './PlatformProvider.jsx';
 export { registerComponent, getComponent } from './ComponentRegistry.js';
+export { useGeminiAudio } from './hooks/useGeminiAudio.js';
