616a1e86b7bee1de27df7c39489cab519495bc1a
GetyeTek <getyeteklu2@gmail.com>
2026-07-07 16:57:22 +0300

    Extract DOM Physics and Gesture mathematics into reusable ho

--- packages/core-sdk/src/index.js
+++ packages/core-sdk/src/index.js
@@ -2,3 +2,4 @@ export { supabase } from './supabaseClient.js';
 export { PlatformProvider, usePlatform } from './PlatformProvider.jsx';
 export { registerComponent, getComponent } from './ComponentRegistry.js';
 export { useGeminiAudio } from './hooks/useGeminiAudio.js';
+export { useGlobalSwipe } from './hooks/useGlobalSwipe.js';
