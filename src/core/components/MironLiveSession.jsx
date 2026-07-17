65e307f6d18f5063d8a48e92031948a60e740124
GetyeTek <getyeteklu2@gmail.com>
2026-07-17 16:59:48 +0300

    Surgically Fix Missing Closing Brace in MironLiveSession con

--- src/core/components/MironLiveSession.jsx
+++ src/core/components/MironLiveSession.jsx
@@ -128,6 +128,7 @@ const MironLiveSession = ({ onClose, mironAvatarUrl }) => {
                 console.error("WS Parse error", err);
             }
         };
+        };
 
         connectWS();
 
